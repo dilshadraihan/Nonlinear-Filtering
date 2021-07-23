a8e8373627a5b06f57d8316292c9e02e3ad5a7ff
Dilshad Raihan <dilshadraihan@gmail.com>
2021-07-23 02:35:02 -0500

    Particle Filter resampling added

--- PFclass.py
+++ PFclass.py
@@ -6,8 +6,7 @@ Created on Wed Apr  7 03:21:45 2021
 @author: dilshad
 """
 import numpy as np
-
-from discreteNLsys1D import discnonlexmp
+from scipy.stats import multivariate_normal as gauspdf
 
 
 
@@ -28,9 +27,18 @@ class PF():
     def PFupdate(self,y_true):
        
         self.samp.measure()
-        #for i in range(n):
-        #    self.w[i]=normpdf(self.samp.obs[k],y_true,self.samp.R)
-        #self.w=self.w/sum(self.w)
+        
+        self.w=gauspdf(self.samp.obs,mean=y_true,cov=self.samp.R)
+        self.w=self.w/sum(self.w)
        
+    def PFresamp(self):
+        
+        ranser=np.random.uniform(0,1,self.samp.N)
+        cumul_wt=np.cumsum(self.w)
+        olstate=self.samp.state
+        for k in range(self.N):
+            jj=np.where(ranser[k]<cumul_wt)[0][0]
+            self.samp.state[i]=olstate[jj]
+        self.w=[1/self.N]*self.N    
       
          
